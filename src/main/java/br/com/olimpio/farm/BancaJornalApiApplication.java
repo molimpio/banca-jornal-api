a9182c0e4e931c4e36059113f0816b9f4bd090a7
Moises <moises@rcbdesenvolvimento.com.br>
2018-04-27 20:14:07 -0300

    iniciou projeto api

---
+++ src/main/java/br/com/olimpio/farm/BancaJornalApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.olimpio.farm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancaJornalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancaJornalApiApplication.class, args);
+	}
+}
