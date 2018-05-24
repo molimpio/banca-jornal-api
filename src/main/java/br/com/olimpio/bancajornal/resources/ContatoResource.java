61ce38b7ba80f653972c70e42d956c1a88351eee
Moises <moises@rcbdesenvolvimento.com.br>
2018-05-24 20:23:41 -0300

    retornando usuario

--- src/main/java/br/com/olimpio/bancajornal/resources/ContatoResource.java
+++ src/main/java/br/com/olimpio/bancajornal/resources/ContatoResource.java
@@ -3,6 +3,7 @@ package br.com.olimpio.bancajornal.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,10 +29,10 @@ public class ContatoResource {
 	}
 	
 	@RequestMapping(value="/contato", method=RequestMethod.POST)
-	public boolean insert(@RequestBody Contato contato) {					
+	public ResponseEntity<Contato> insert(@RequestBody Contato contato) {					
 		contato.setId(null);				
 		contatoRepositoy.save(contato);
-		return true;		
+		return ResponseEntity.ok(contato);
 	}
 
 }
